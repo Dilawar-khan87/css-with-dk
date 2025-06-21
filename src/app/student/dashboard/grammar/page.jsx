88037842d1b8ca4518d4cdc89294be8d1fccb929
Dilawar-khan87 <dilawarkhanlakho@gmail.com>
2025-06-21 17:31:35 +0500

    text color fixing 1

--- src/app/student/dashboard/grammar/page.jsx
+++ src/app/student/dashboard/grammar/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-function page() {
+function Page() {
   return (
-    <div className=" items-center justify-center  bg]-gray-100 p-6">
-      <h1 className="text-4xl font-bold mb-4">Coming Soon....</h1>
+    <div className="flex items-center justify-center bg-gray-100 p-6 min-h-screen">
+      <h1 className="text-4xl font-bold mb-4 text-blue-800">
+        Coming Soon...
+      </h1>
     </div>
   );
 }
 
-export default page;
+export default Page;
